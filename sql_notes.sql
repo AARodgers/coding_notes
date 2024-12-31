0b3537f75886c6de399d71c8143ddf296dfa3310
Amanda Rodgers <aarodgerswork@gmail.com>
2024-12-31 15:29:31 -0700

    Add subqueries for fake HR database

--- sql_notes.sql
+++ sql_notes.sql
@@ -303,3 +303,107 @@ SELECT SUM(QUANTITY) FROM PETRESCUE WHERE MONTH(RESCUEDATE)="05";
 
 --The rescue shelter is supposed to find good homes for all animals within 1 year of their rescue. Write a query that displays the ID and the target date.
 SELECT ID, DATE_ADD(RESCUEDATE, INTERVAL 1 YEAR) FROM PETRESCUE;
+
+
+-- Subqueries
+--Write SQL queries that demonstrate the necessity of using sub-queries
+--Compose sub-queries in the where clause
+--Build column expressions (for example, sub-query in place of a column)
+--Write table expressions (for example, sub-query in place of a table)
+
+-- SQL script to create fake HR tables
+CREATE TABLE EMPLOYEES (
+                            EMP_ID CHAR(9) NOT NULL,
+                            F_NAME VARCHAR(15) NOT NULL,
+                            L_NAME VARCHAR(15) NOT NULL,
+                            SSN CHAR(9),
+                            B_DATE DATE,
+                            SEX CHAR,
+                            ADDRESS VARCHAR(30),
+                            JOB_ID CHAR(9),
+                            SALARY DECIMAL(10,2),
+                            MANAGER_ID CHAR(9),
+                            DEP_ID CHAR(9) NOT NULL,
+                            PRIMARY KEY (EMP_ID));
+
+  CREATE TABLE JOB_HISTORY (
+                            EMPL_ID CHAR(9) NOT NULL,
+                            START_DATE DATE,
+                            JOBS_ID CHAR(9) NOT NULL,
+                            DEPT_ID CHAR(9),
+                            PRIMARY KEY (EMPL_ID,JOBS_ID));
+
+ CREATE TABLE JOBS (
+                            JOB_IDENT CHAR(9) NOT NULL,
+                            JOB_TITLE VARCHAR(30),
+                            MIN_SALARY DECIMAL(10,2),
+                            MAX_SALARY DECIMAL(10,2),
+                            PRIMARY KEY (JOB_IDENT));
+
+CREATE TABLE DEPARTMENTS (
+                            DEPT_ID_DEP CHAR(9) NOT NULL,
+                            DEP_NAME VARCHAR(15) ,
+                            MANAGER_ID CHAR(9),
+                            LOC_ID CHAR(9),
+                            PRIMARY KEY (DEPT_ID_DEP));
+
+CREATE TABLE LOCATIONS (
+                            LOCT_ID CHAR(9) NOT NULL,
+                            DEP_ID_LOC CHAR(9) NOT NULL,
+                            PRIMARY KEY (LOCT_ID,DEP_ID_LOC));
+
+Fake HR Database instructions:
+
+Load the database
+Using the skills acquired in the previous modules, you should first create the database in MySQL. Follow the steps below.
+
+Open the phpMyAdmin interface from the Skills Network Toolbox in Cloud IDE.
+
+Create a blank database named 'HR'. Use the script shared in the link below to create the required tables.
+Script_Create_Tables.sql
+
+Download the files in the links below to your local device (if not already done in previous labs)
+Departments.csv
+Jobs.csv
+JobsHistory.csv
+Locations.csv
+Employees.csv
+
+Use these files in the phpMyAdmin interface as the data for the respective tables in the 'HR' database.
+
+Sub-queries and Nested Selects
+Say you are asked to retrieve all employee records whose salary is lower than the average salary. You might use the following query to do this.
+
+
+SELECT *
+FROM EMPLOYEES
+WHERE salary < AVG(salary);
+
+However, this query will generate an error stating, "Illegal use of group function." Here, the group function is AVG and cannot be used directly in the condition since it has not been retrieved from the data. Therefore, the condition will use a sub-query to retrieve the average salary information to compare the existing salary. The modified query would become:
+
+
+SELECT *
+FROM EMPLOYEES
+WHERE SALARY < (SELECT AVG(SALARY) FROM EMPLOYEES);
+
+Now, consider executing a query that retrieves all employee records with EMP_ID, SALARY, and maximum salary as MAX_SALARY in every row. For this, the maximum salary must be queried and used as one of the columns. This can be done using the query below.
+
+
+SELECT EMP_ID, SALARY, (SELECT MAX(SALARY) FROM EMPLOYEES) AS MAX_SALARY
+FROM EMPLOYEES;
+
+Now, consider that you wish to extract the first and last names of the oldest employee. Since the oldest employee will be the one with the smallest date of birth, the query can be written as:
+
+SELECT F_NAME, L_NAME
+FROM EMPLOYEES
+WHERE B_DATE = (SELECT MIN(B_DATE) FROM EMPLOYEES);
+Copied!
+You may also use sub-queries to create derived tables, which can then be used to query specific information. Say you want to know the average salary of the top 5 earners in the company. You will first have to extract a table of the top five salaries as a table. From that table, you can query the average value of the salary. The query can be written as follows.
+
+SELECT AVG(SALARY)
+FROM (SELECT SALARY
+      FROM EMPLOYEES
+      ORDER BY SALARY DESC
+      LIMIT 5) AS SALARY_TABLE;
+
+Note that it is necessary to give an alias to any derived tables.
