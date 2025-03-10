2e4a6f7a526fc4122284aade383cb56fd9358fd7
Amanda Rodgers <aarodgerswork@gmail.com>
2025-03-10 13:42:15 -0400

    Add linux notes

--- linux/linux_notes.py
+++ linux/linux_notes.py
@@ -11,4 +11,27 @@ Symbol	Stands for path to
 .	Current directory
 ..	Parent directory
 
+Go to home directory:
+cd ~
+
+Change to parent directory:
+cd ..
+
+Change to root directory;
+cd /
+
+Change to a subdirectory:
+cd <name_of_subdirectory>
+
+Change directories:
+cd ./bin
+(the . period means the path to your current working directory)
+(if you are already in bin, it will try to take you to /bin/bin)
+
+Change to a sibling directory that is at the same level in the current directory:
+cd ../<directory_name>
+
+Open text editor nano:
+nano file_name
+
 
