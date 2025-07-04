1d3dc5b671f49bacb3b302836b4d98960b418ec0
Amanda Rodgers <aarodgerswork@gmail.com>
2025-07-04 18:10:30 -0400

    Add shell variable notes

--- shell_scripting/shell_scripting.py
+++ shell_scripting/shell_scripting.py
@@ -21,3 +21,33 @@ chmod +x hello_world.sh
 
 # Execute script (should print 'hello world') in command line
 ./hello_world.sh
+
+# Shell variables offer a powerful way to store and later access or modify information such as numbers, character strings, and other data structures by name.
+$ firstname=Jeff
+$ echo $firstname
+Jeff
+
+# The first line assigns the value Jeff to a new variable called firstname.
+# The next line accesses and displays the value of the variable, using the echo command
+# along with the special character $ in front of the variable name to extract its value,
+# which is the string
+
+# Another way to create a shell variable using read command
+$ read lastname
+# on the command line, the shell waits for you to enter some text:
+$ read lastname
+Grossman
+# Now we can see that the value Grossman has just been stored in the variable lastname by the read command:
+$ read lastname
+Grossman
+$ echo $lastname
+Grossman
+
+# you can echo the values of multiple variables at once.
+echo $firstname $lastname
+
+# As you will soon see, the read command is particularly useful in shell scripting. Y
+# You can use it within a shell script to prompt users to input information,
+# which is then stored in a shell variable and available for use by the shell script while it is running.
+# You will also learn about command line arguments, which are values that can be passed to a script and
+# automatically assigned to shell variables.
