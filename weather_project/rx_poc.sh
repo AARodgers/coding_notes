34ed97655b40a979c30e13f1e7ccfc40805e513d
Amanda Rodgers <aarodgerswork@gmail.com>
2025-10-10 17:51:16 -0600

    Add temp to script

--- weather_project/rx_poc.sh
+++ weather_project/rx_poc.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 curl -s "wttr.in/$city?T" --output weather_report
+
+#To extract Current Temperature
+obs_temp=$(curl -s "wttr.in/$city?T" | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $obs_temp"
+
+# To extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s "wttr.in/$city?T" | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
