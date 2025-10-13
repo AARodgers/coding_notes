1d07513558ebce6b3f7fc24049192fd3a78d50e9
Amanda Rodgers <aarodgerswork@gmail.com>
2025-10-13 11:24:56 -0600

    Echo min and max error

--- weather_project/weekly_stats.sh
+++ weather_project/weekly_stats.sh
@@ -8,3 +8,26 @@ week_fc=($(echo $(cat scratch.txt)))
 for i in {0..6}; do
     echo ${week_fc[$i]}
 done
+
+for i in {0..6}; do
+  if [[ ${week_fc[$i]} < 0 ]]
+  then
+    week_fc[$i]=$(((-1)*week_fc[$i]))
+  fi
+  # validate result:
+  echo ${week_fc[$i]}
+done
+minimum=${week_fc[1]}
+maximum=${week_fc[1]}
+for item in ${week_fc[@]}; do
+   if [[ $minimum > $item ]]
+   then
+     minimum=$item
+   fi
+   if [[ $maximum < $item ]]
+   then
+     maximum=$item
+   fi
+done
+echo "minimum ebsolute error = $minimum"
+echo "maximum absolute error = $maximum"
