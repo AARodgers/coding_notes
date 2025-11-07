e0c760c824a320e1121030e874b8c70b52d7e3d4
Amanda Rodgers <aarodgerswork@gmail.com>
2025-11-06 17:38:25 -0700

    Add dba notes

---
+++ DBA/mysql_lab.sql
@@ -0,0 +1,16 @@
+-- use the MySQL Command Line Interface (CLI) to carry out a variety of functions related to selecting
+--and understanding some of the alternative storage engines available in MySQL. You will then continue on to explore the system tables in MySQL which contain meta data about the objects in the server.
+
+--Create tables using alternative storage engines.
+--Query MySQL system tables to retrieve meta data about objects in the database.
+
+--Create a new database world:
+CREATE DATABASE world;
+
+--To use the newly created world database, use the command below in the terminal:
+USE world;
+
+--Execute the world mysql script (world_mysql.sql) to complete the world database creation process:
+SOURCE /path/to/your/world_mysql.sql;
+
+
