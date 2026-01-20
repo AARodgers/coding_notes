ff8393980d9ffbc5112364ab3084749735df8acc
Amanda Rodgers <aarodgerswork@gmail.com>
2026-01-20 16:18:25 -0700

    Add employee db and notes

---
+++ DBA/slow_query_lab.sql
@@ -0,0 +1,22 @@
+-- Objectives:
+-- Use the EXPLAIN statement to check the performance of your query
+-- Add indexes to improve the performance of your query
+-- Apply other best practices such as using the UNION ALL clause to improve query performance
+
+--  source: https://dev.mysql.com/doc/employee/en/ under the CC BY-SA 3.0 License
+
+-- To download employee database:
+-- wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0231EN-SkillsNetwork/datasets/employeesdb.zip
+
+-- Unzip file
+-- unzip employeesdb.zip
+
+-- change directories so that we’re able to access the files in the newly created employeesdb folder.
+-- cd employeesdb
+
+-- Have to put the database into your MySQL instance ( need to start on first):
+
+-- switch to mysql CLI or prompt and go to the db you will be using:
+-- use employees;
+
+
