84e7435021fb8060b3b6838dcc99d92b346bf88f
Amanda Rodgers <aarodgerswork@gmail.com>
2026-02-18 11:03:52 -0700

    Add tr notes

--- shell_scripting/etl_shell_script.sh
+++ shell_scripting/etl_shell_script.sh
@@ -28,3 +28,37 @@ cut -d":" -f1,3,6 /etc/passwd
 
 # The command below extracts a range of fields 3rd to 6th (userid, groupid, user description and home directory) from /etc/passwd.
 cut -d":" -f3-6 /etc/passwd
+
+### Transforming data using 'tr'
+# tr is a filter command used to translate, squeeze, and/or delete characters.
+
+# 1. Translate from one character set to another
+# The command below translates all lower case alphabets to upper case.
+echo "Shell Scripting" | tr "[a-z]" "[A-Z]"
+
+# You could also use the pre-defined character sets also for this purpose:
+
+echo "Shell Scripting" | tr "[:lower:]" "[:upper:]"
+
+#The command below translates all upper case alphabets to lower case.
+
+echo "Shell Scripting" | tr  "[A-Z]" "[a-z]"
+
+
+## Squeeze repeating occurrences of characters
+# The -s option replaces a sequence of a repeated characters with a single occurrence of that character.
+
+# The command below replaces repeat occurrences of ‘space’ in the output of ps command with one ‘space’.
+
+ps | tr -s " "
+
+# In the above example, the space character within quotes can be replaced with the following : "[\:space\:]".
+
+# Delete characters
+# We can delete specified characters using the -d  option.
+
+# The command below deletes all digits.
+
+echo "My login pin is 5634" | tr -d "[:digit:]"
+
+# The output will be : ‘My login pin is’
