aae852defb83c55d0a68c691ef13d0be006903c9
Amanda Rodgers <aarodgerswork@gmail.com>
2026-03-25 10:44:44 -0600

    Add optional_kafka_msg_offstes.py

---
+++ kafka/optional_kafka_msg_keys_offsets.py
@@ -0,0 +1,19 @@
+# Kafka Message Keys and Offset
+# In this lab, you will keep the message streams added to the partitions in topics in a Kafka broker sorted in their original publication order and state using offset and groups.
+# You will learn:
+# Use message keys to keep message streams sorted in their original publication state and order
+
+# Use consumer offset to control and track message sequential positions in topic partitions
+
+#Run the commands below on the newly opened terminal.
+Note: You can copy the code by clicking the little copy button on the bottom right of the code below and then paste it, wherever you wish.
+
+#Download Kafka by running the command below:
+wget https://archive.apache.org/dist/kafka/3.8.0/kafka_2.13-3.8.0.tgz
+
+#Extract Kafka from the zip file by running the command below.
+tar -xzf kafka_2.13-3.8.0.tgz
+
+#Note: This command creates a directory named kafka_2.13-3.8.0 in the current directory.
+
+
