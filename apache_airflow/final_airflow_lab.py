12ae42cd327cc337f1315c35e98c380e8847ab08
Amanda Rodgers <aarodgerswork@gmail.com>
2026-03-28 12:52:56 -0600

    Add comma to args

--- apache_airflow/final_airflow_lab.py
+++ apache_airflow/final_airflow_lab.py
@@ -304,5 +304,100 @@ unzip_data > extract_data_from_csv > extract_data_from_tsv > extract_data_from_f
 
 #### End Code #####
 
+# In terminal:
+
+# Submit dag ( just paste dag file into dag directory)
+cp /home/project/ETL_toll_data.py /home/project/airflow/dags
+
+
+
 mkdir -p /home/project/staging
 tar -xvzf /home/project/tolldata.tgz -C /home/project/staging
+
+
+######## ETL_toll_data.py code #######
+
+# import the libraries
+from datetime import timedelta
+# To instantiate a DAG
+from airflow.models import DAG
+
+# install operators
+from airflow.operators.bash_operator import BashOperator
+
+# Define DAG arguments
+default_args = {
+    'owner': 'Bob',
+    'start_date': days_ago(0),
+    'email': ['dummy@email.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+dag = DAG(
+    dag_id='ETL_toll_data',
+    schedule_interval=timedelta(days=1),
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+)
+
+# define task1 unzip_data
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -xvzf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment/staging',
+    dag=dag,
+)
+
+# define task2 extract_data_from_csv
+extract_data_from_csv = BashOperator(
+    task_id='extract_data_from_csv',
+    bash_command="cut -d',' -f1,2,3,4 /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv > /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv",
+    dag=dag,
+)
+
+# define task3 (2.3) extract_data_from_csv
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command="cut -d',' -f5,6,7 /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/staging/tsv_data.csv",
+    dag=dag,
+)
+
+
+# define task4 (2.4) extract_data_from_fixed_width
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command="cut -d',' -f6,7 /home/project/airflow/dags/finalassignment/staging/payment-data.txt > /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv",
+    dag=dag,
+)
+
+# define task5 (2.5) consolidate_data
+consolidate_data = BashOperator(
+    task_id='consolidate_data',
+    bash_command="""
+        paste -d',' \
+        /home/project/airflow/dags/finalassignment/csv_data.csv \
+        /home/project/airflow/dags/finalassignment/tsv_data.csv \
+        /home/project/airflow/dags/finalassignment/fixed_width_data.csv \
+        > /home/project/airflow/dags/finalassignment/extracted_data.csv
+    """,
+    dag=dag,
+)
+
+# define task6 (2.6) transform_data
+transform_data = BashOperator(
+    task_id='transform_data',
+    bash_command="""
+        awk -F',' 'BEGIN {OFS=","} {$4=toupper($4); print}' \
+        /home/project/airflow/dags/finalassignment/extracted_data.csv \
+        > /home/project/airflow/dags/finalassignment/transformed_data.csv
+    """,
+    dag=dag,
+)
+
+# Define task pipeline (2.7)
+unzip_data > extract_data_from_csv > extract_data_from_tsv > extract_data_from_fixed_width > consolidate_data > transform_data
+
+#######################################
